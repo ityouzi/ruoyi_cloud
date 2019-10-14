12fbc287dbea8e377f0a4aecd548c5634a36c47a
Administrator <2504480136@qq.com>
2019-10-14 17:49:15 +0800

    更新common模块

---
+++ ruoyi_common/ruoyi_common_core/src/main/java/com/ityouzi/constant/Constants.java
@@ -0,0 +1,75 @@
+package com.ityouzi.constant;
+/**
+ * 通用常量信息
+ */
+public class Constants {
+
+    /**
+     * UTF-8 字符集
+     */
+    public static final String UTF8 = "UTF-8";
+
+    /**
+     * 通用成功标识
+     */
+    public static final String SUCCESS = "0";
+
+    /**
+     * 通用失败标识
+     */
+    public static final String FAIL = "1";
+
+    /**
+     * 登录成功
+     */
+    public static final String LOGIN_SUCCESS = "Success";
+
+    /**
+     * 注销
+     */
+    public static final String LOGOUT = "Logout";
+
+    /**
+     * 登录失败
+     */
+    public static final String LOGIN_FAIL = "Error";
+
+    /**
+     * 自动去除表前缀
+     */
+    public static final String AUTO_REMOVE_PRE = "true";
+
+    /**
+     * 当前记录起索引
+     */
+    public static final String PAGE_NUM = "pageNum";
+
+    /**
+     * 每页显示记录数
+     */
+    public static final String PAGE_SIZE = "pageSize";
+
+    /**
+     * 排序
+     */
+    public static final String ORDER_BY_COLUMN = "sortField";
+
+    /**
+     * 排序方式"desc" or "asc"
+     */
+    public static final String IS_ASC = "sortOrder";
+
+    public static final String CURRENT_ID       = "current_id";
+
+    public static final String CURRENT_USERNAME = "current_username";
+
+    public static final String TOKEN            = "token";
+
+    public static final String DEFAULT_CODE_KEY = "random_code_";
+
+    public final static String ACCESS_TOKEN     = "access_token_";
+
+    public final static String ACCESS_USERID    = "access_userid_";
+
+    public static final String RESOURCE_PREFIX  = "/profile";
+}
